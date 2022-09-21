34d5c0adb0ca82ef5080c62b5da122c072e3a945
Mikhail Soldatkin <michael.soldatkin@gmail.com>
2022-09-21 13:05:39 +0300

    models created

--- api/models.py
+++ api/models.py
@@ -1,3 +1,55 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Country(models.Model):
+    name = models.CharField('Название')
+
+    def __str__(self):
+        return self.name
+
+
+class Manufacturer(models.Model):
+    name = models.CharField('Название')
+    country = models.ForeignKey(
+        Country,
+        related_name='manufacturers',
+        on_delete=models.SET_NULL,
+        null=True,
+        verbose_name='Страна',
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Car(models.Model):
+    name = models.CharField('Название')
+    manufacturer = models.ForeignKey(
+        Manufacturer,
+        related_name='cars',
+        on_delete=models.CASCADE,
+        verbose_name='Производитель',
+    )
+    start_year = models.PositiveIntegerField('Год начала выпуска')
+    end_year = models.PositiveIntegerField('Год окончания выпуска')
+
+    def __str__(self):
+        return self.name
+
+
+class Comment(models.Model):
+    author_email = models.EmailField('Email Автора')
+    car = models.ForeignKey(
+        Car,
+        related_name='comments',
+        on_delete=models.CASCADE,
+        verbose_name='Автомобиль',
+    )
+    created = models.DateTimeField('Дата создания', auto_now_add=True)
+    text = models.TextField('Текст комментария')
+
+    def __str__(self):
+        return self.text[:15]
